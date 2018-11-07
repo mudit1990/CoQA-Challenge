0ca6dbe9ec088135fe175c9701105368962c0298
Abhishek Chinni <abhishek@Abhisheks-MacBook-Pro.local>
2018-11-07 15:34:15 -0500

    mycommit

--- reader.py
+++ reader.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 from typing import Dict, List, Tuple
@@ -21,8 +20,27 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 # commands.main()
 
 
-@DatasetReader.register("coqa1")
-class MyDatasetReader(DatasetReader):
+@DatasetReader.register("coqa-squad-plus")
+class SquadReaderPlus(DatasetReader):
+    """
+    Reads a JSON-formatted SQuAD file and returns a ``Dataset`` where the ``Instances`` have four
+    fields: ``question``, a ``TextField``, ``passage``, another ``TextField``, and ``span_start``
+    and ``span_end``, both ``IndexFields`` into the ``passage`` ``TextField``.  We also add a
+    ``MetadataField`` that stores the instance's ID, the original passage text, gold answer strings,
+    and token offsets into the original passage, accessible as ``metadata['id']``,
+    ``metadata['original_passage']``, ``metadata['answer_texts']`` and
+    ``metadata['token_offsets']``.  This is so that we can more easily use the official SQuAD
+    evaluation script to get metrics.
+    Parameters
+    ----------
+    tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
+        We use this ``Tokenizer`` for both the question and the passage.  See :class:`Tokenizer`.
+        Default is ```WordTokenizer()``.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        We similarly use this for both the question and the passage.  See :class:`TokenIndexer`.
+        Default is ``{"tokens": SingleIdTokenIndexer()}``.
+    """
+
     def __init__(self,
                  tokenizer: Tokenizer = None,
                  token_indexers: Dict[str, TokenIndexer] = None,
@@ -35,6 +53,7 @@ class MyDatasetReader(DatasetReader):
     def _read(self, file_path: str):
         # if `file_path` is a URL, redirect to the cache
         file_path = cached_path(file_path)
+
         logger.info("Reading file at %s", file_path)
         with open(file_path) as dataset_file:
             dataset_json = json.load(dataset_file)
